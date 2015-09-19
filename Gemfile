b31b42929abb6b57c12684ffef060a977765cba0
Chetan <s.p.chetan11@gmail.com>
2015-09-20 02:55:48 +0530

    spree frontend local push

--- Gemfile
+++ Gemfile
@@ -49,5 +49,6 @@ end
 
 
 gem 'spree', '3.0.4'
+gem 'spree-frontend', '3.0.4', path: 'vendor/spree_frontend'
 gem 'spree_gateway', github: 'spree/spree_gateway', branch: '3-0-stable'
 gem 'spree_auth_devise', github: 'spree/spree_auth_devise', branch: '3-0-stable'
--- config/application.rb
+++ config/application.rb
@@ -5,7 +5,7 @@ require 'rails/all'
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
-
+config.assets.initialize_on_precompile = false
 module StubfairSpree
   class Application < Rails::Application
     
@@ -35,6 +35,6 @@ module StubfairSpree
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
-    config.assets.initialize_on_precompile = false
+
   end
 end
